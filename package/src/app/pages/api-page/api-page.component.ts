8c735eb91f1ec812990610a0942444237604dd32
Juan <jestebanl2004@gmail.com>
2025-03-04 10:15:44 +0100

    Configurado el get de la api

--- package/src/app/pages/api-page/api-page.component.ts
+++ package/src/app/pages/api-page/api-page.component.ts
@@ -1,19 +1,24 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { MaterialModule } from '../../material.module';
-import { AppSalesOverviewComponent } from 'src/app/components/sales-overview/sales-overview.component';
-import { AppDailyActivitiesComponent } from 'src/app/components/daily-activities/daily-activities.component';
-import { AppProductPerformanceComponent } from 'src/app/components/product-performance/product-performance.component';
-import { AppBlogComponent } from 'src/app/components/apps-blog/apps-blog.component';
+import { ApiServiceService } from 'src/app/services/api-service.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-api-page',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './api-page.component.html',
   styleUrl: './api-page.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export default class ApiPageComponent {
 
-  constructor() { }
+  public datos:any[]=[];
+  constructor(private servicio:ApiServiceService) { }
+
+  ngOnInit(): void {
+    this.servicio.getDatos().subscribe(
+      (datos) => {
+        this.datos=datos;
+      });
+  }
 
 }
--- package/src/app/services/api-service.service.ts
+++ package/src/app/services/api-service.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  private endpoint:string="https://fakestoreapiserver.reactbd.com/photos";
+
+  constructor(private http:HttpClient) { }
+
+  public getDatos():Observable<any[]>{
+    return this.http.get<any[]>(this.endpoint); //select
+  }
 }
